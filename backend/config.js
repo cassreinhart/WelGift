f373a1339772020ff10ad05a3dbbd3e06aa25e7e
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-10-08 10:24:53 -0500

    added auth routes

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@
 /** Shared config for application; can be required many places. */
 
 require("dotenv").config();
-require("colors");
+// require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
---
+++ backend/routes/auth.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/** Routes for authentication. */
+
+const jsonschema = require("jsonschema");
+
+const User = require("../models/user");
+const express = require("express");
+const router = new express.Router();
+const { createToken } = require("../helpers/tokens");
+const userAuthSchema = require("../schemas/userAuth.json");
+const userRegisterSchema = require("../schemas/userRegister.json");
+const { BadRequestError } = require("../expressError");
+
+/** POST /auth/token:  { username, password } => { token }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+router.post("/token", async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, userAuthSchema); //is there username & pwd in the req.body?
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const { username, password } = req.body;
+    const user = await User.authenticate(username, password); //check password & hashed pwd
+    const token = createToken(user); //create a token for the user
+    return res.json({ token }); //give back the token in the response
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+/** POST /auth/register:   { user } => { token }
+ *
+ * user must include { username, password, firstName, lastName, email }
+ *
+ * Returns JWT token which can be used to authenticate further requests.
+ *
+ * Authorization required: none
+ */
+
+router.post("/register", async function (req, res, next) {
+  try {
+    console.log(req.body)
+    const validator = jsonschema.validate(req.body, userRegisterSchema); //requires all fields to validate (length req & email format check)
+    if (!validator.valid) {
+      console.log("INVALID REQUEST")
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const newUser = await User.register({ ...req.body, isAdmin: false });
+    const token = createToken(newUser);
+    return res.status(201).json({ token });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+module.exports = router;
