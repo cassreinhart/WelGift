59f74fad99cebf7a3ed037e7b5d5051c6d72672b
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-10-24 19:45:01 -0500

    update routes and config

--- backend/config.js
+++ backend/config.js
@@ -3,7 +3,7 @@
 /** Shared config for application; can be required many places. */
 
 require("dotenv").config();
-// require("colors");
+require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
@@ -11,9 +11,10 @@ const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test")
-      ? "welgift_test"
-      : process.env.DATABASE_URL || "welgift";
+  return "welgift_test"
+  // return (process.env.NODE_ENV === "test")
+  //     ? "welgift_test"
+  //     : process.env.DATABASE_URL || "welgift";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
