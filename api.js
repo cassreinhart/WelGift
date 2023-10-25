037a90c3452e2bfda5566d399ec9be979eed8fde
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-10-24 19:49:39 -0500

    clarification

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 /** API Class.
  *
- * Static class tying together methods used to get/send to to the API.
+ * Static class tying together methods used to communicate with the API.
  *
  */
 
@@ -15,8 +15,7 @@ class WelgiftApi {
   static async request(endpoint, data = {}, method = "get") {
     console.debug("API Call:", endpoint, data, method);
 
-    //there are multiple ways to pass an authorization token, this is how you pass it in the header.
-    //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
+    //pass an authorization token in the header.
     const url = `${BASE_URL}/${endpoint}`;
     const headers = { Authorization: `Bearer ${WelgiftApi.token}` };
     const params = (method === "get")
@@ -73,7 +72,7 @@ class WelgiftApi {
   }
 }
 
-// for now, put token ("testuser" / "password" on class)
+// for testing: ("testuser" / "password" on class)
 WelgiftApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
